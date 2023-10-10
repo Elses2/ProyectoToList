a3a813ecc2172749d0aca44e3200b1f33349499f
eber <eberchiecher2@gmail.com>
2023-10-10 01:05:59 -0300

    Programa funcional, falta corregir detalles

--- index.js
+++ index.js
@@ -1,180 +1,204 @@
-const tarea= require('./Tarea');
-const ingreso_datos=require('readline-sync');
-function presentacion(){
+const tarea = require('./Tarea');
+const ingreso_datos = require('readline-sync');
 
-	console.log("1 Ver tarea ");
-	console.log("2 Buscar Tarea");
-	console.log("3 Agregar Tarea");
-	console.log("4 Cerrar ");
-	
+// Función que muestra el menú de opciones.
+function presentacion() {
+    console.log("1 Ver tarea ");
+    console.log("2 Buscar Tarea");
+    console.log("3 Agregar Tarea");
+    console.log("4 Cerrar ");
 }
-function editar_elemento_complemento(lista,indice){
-	const elemento= lista[indice-1];
-	
 
-}
-function agregar_elemento(){
-	let titulo,descripcion,vencimiento,dificultad;
-	titulo=ingreso_datos.question("Titulo: \n");
-	descripcion=ingreso_datos.question("Descripcion: \n");
-	estado=ingreso_datos.questionInt("Estado(1pendiente,2en curso,3finalizado): \n");
-	do{
-			const fechaTexto = ingreso_datos.question('Ingresa fecha de Vencimiento (YYYY-MM-DD): ');
-			// Intentar convertir la cadena a un objeto Date
-			vencimiento = new Date(fechaTexto);
-	}while(isNaN(vencimiento.getTime()));
-	Dificultad=ingreso_datos.questionInt("Dificultad(1-facil 2medio 3dificil): \n"); 
-	return new tarea(titulo,descripcion,vencimiento,dificultad);
+// Función para editar un elemento complemento 
+function editar_elemento_complemento(lista, indice) {
+    const elemento = lista[indice - 1];
 
 }
-function detalle_tarea(lista, seleccion){
-	let desicion=0;
-	seleccion=ingreso_datos.questionInt("Si deseas ver en detalle una de estas tareas selecciona su indice, si no, preciona 0 u otra tecla distinta de los indices existentes\n");
-	if(seleccion <=lista.length && seleccion>0){
-		detalle_tarea_complemento(lista, seleccion)
-					}
-	desicion=ingreso_datos.question("Si deseas modificar esta tarea preciona: e   si no preciona otra tecla\n");
-	if(desicion==e){
-		lista[seleccion].editar();
-	}
-}
-function CasoUno(){
 
-	console.log("1 Todo");
-	console.log("2 Pendiente");
-	console.log("3 En Curso");
-	console.log("4 Finalizado");
-	console.log("5 Volver");
-	
+// Función para agregar un elemento a la lista.
+function agregar_elemento() {
+    let titulo, descripcion, vencimiento, dificultad;
+    titulo = ingreso_datos.question("Titulo: \n");
+    descripcion = ingreso_datos.question("Descripcion: \n");
+    do {
+        const fechaTexto = ingreso_datos.question('Ingresa fecha de Vencimiento (YYYY-MM-DD):\n');
+        // Intentar convertir la cadena a un objeto Date
+        vencimiento = new Date(fechaTexto);
+    } while (isNaN(vencimiento.getTime()));
+    dificultad = ingreso_datos.questionInt("Dificultad(1-facil 2-medio 3-dificil): \n");
+    return new tarea(titulo, descripcion, vencimiento, dificultad);
 }
+
+
+// Función para ver los detalles de una tarea.
+function detalle_tarea(lista) {
+    let desicion = 0;
+    let seleccion = ingreso_datos.questionInt("Si deseas ver en detalle una de estas tareas selecciona su indice, si no, presiona 0 u otra tecla distinta de los indices existentes\n");
+    if (seleccion <= lista.length && seleccion > 0) {
+        detalle_tarea_complemento(lista, seleccion);
+    }
+    desicion = ingreso_datos.question("Si deseas modificar esta tarea presiona: e   si no, presiona otra tecla\n");
+    if (desicion === "e") {
+        lista[seleccion - 1].editar();
+    }
+}
+
+
+// Función para mostrar los detalles de una tarea.
 function detalle_tarea_complemento(lista, indice) {
-  const elemento = lista[indice-1];
-  console.log(`Esta es la tarea que elegiste\n ${elemento.titulo}`);
-  console.log(`${elemento.descripcion}`);
-  console.log(`estado: ${elemento.estado}`);
-  console.log(`dificultad: ${elemento.dificultad}`);
-  console.log(`creacion: ${elemento.creacion}`);
-  console.log(`ultima edicion: ${elemento.ultima_edicion}`);
-  console.log(`vencimiento: ${elemento.vencimiento}`);
- 
+    const elemento = lista[indice - 1];
+    console.log("Detalles de la tarea:");
+    console.log(`Título: ${elemento.titulo}`);
+    console.log(`Descripción: ${elemento.descripcion}`);
+    console.log(`Estado: ${elemento.estado}`);
+    console.log(`Dificultad: ${elemento.dificultad}`);
+    console.log(`Fecha de Creación: ${formatDate(elemento.creacion)}`);
+    console.log(`Última Edición: ${formatDate(elemento.ultima_edicion)}`);
+    console.log(`Fecha de Vencimiento: ${formatDate(elemento.vencimiento) || 'No especificada'}`);
 }
 
-function verLista_todo(lista){
-	if(lista.length>0){
-	 lista.forEach((elemento, index) => {
- 	 console.log(`${index + 1}. ${elemento.titulo}`);
-	});
-	}
-	else{console.log("La agenda esta vacia");}
-
+// Función para formatear una fecha.
+function formatDate(dateString) {
+    if (!dateString) {
+        return '';
+    }
+    const date = new Date(dateString);
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 }
-function verLista_pendiente(lista){
-	if(lista.length>0){
-	 lista.forEach((elemento, index) => {
- 	 if(elemento.estado === 1){
- 	 	console.log(`${index + 1}. ${elemento.titulo}`);
- 	 }
-	});
-	}
-	else{console.log("La agenda esta vacia");}
-
-}
-function verLista_curso(lista){
-	if(lista.length>0){
-	 lista.forEach((elemento, index) => {
- 	 if(elemento.estado === 2){
- 	 	console.log(`${index + 1}. ${elemento.titulo}`);
- 	 }
-	});
-	}
-	else{console.log("La agenda esta vacia");}
-
-}
-function verLista_terminado(lista){
-	 if(lista.length>0){
-	 lista.forEach((elemento, index) => {
- 	 if(elemento.estado === 3){
- 	 	console.log(`${index + 1}. ${elemento.titulo}`);
- 	 }
-	});
-	}
-	else{console.log("La agenda esta vacia");}
-
+// Función que muestra un submenú para el Caso 1.
+function CasoUno() {
+    console.log("1 Todo");
+    console.log("2 Pendiente");
+    console.log("3 En Curso");
+    console.log("4 Finalizado");
+    console.log("5 Volver");
 }
 
 
+// Función para ver la lista de tareas en estado "Todo".
+function verLista_todo(lista) {
+    if (lista.length > 0) {
+        lista.forEach((elemento, index) => {
+            console.log(`${index + 1}. ${elemento.titulo}`);
+        });
+    } else {
+        console.log("La agenda está vacía");
+    }
+}
 
-let ingreso=0;
-let condicion=0;
-let CondicionCasoUno=0;
-let seleccion=0;
-const lista=[];
+// Función para ver la lista de tareas en estado "Pendiente".
+function verLista_pendiente(lista) {
+    if (lista.length > 0) {
+        lista.forEach((elemento, index) => {
+            if (elemento.estado === 1) {
+                console.log(`${index + 1}. ${elemento.titulo}`);
+            }
+        });
+    } else {
+        console.log("La agenda está vacía");
+    }
+}
+
+// Función para ver la lista de tareas en estado "En Curso".
+function verLista_curso(lista) {
+    if (lista.length > 0) {
+        lista.forEach((elemento, index) => {
+            if (elemento.estado === 2) {
+                console.log(`${index + 1}. ${elemento.titulo}`);
+            }
+        });
+    } else {
+        console.log("La agenda está vacía");
+    }
+}
+
+// Función para ver la lista de tareas en estado "Finalizado".
+function verLista_terminado(lista) {
+    if (lista.length > 0) {
+        lista.forEach((elemento, index) => {
+            if (elemento.estado === 3) {
+                console.log(`${index + 1}. ${elemento.titulo}`);
+            }
+        });
+    } else {
+        console.log("La agenda está vacía");
+    }
+}
+
+let ingreso = 0;
+let condicion = 0;
+let CondicionCasoUno = 0;
+let seleccion = 0;
+const lista = [];
 
 console.log("Bienvenido");
 
-do{
+do {
+    presentacion();
+    ingreso = ingreso_datos.questionInt("Dime Cual de estas opciones deseas:\n")
+    switch (ingreso) {
+        case 1:
+            do {
+                CasoUno();
+                ingreso = ingreso_datos.questionInt("Dime Cual de estas opciones deseas:\n")
+                switch (ingreso) {
+                    case 1:
+                        verLista_todo(lista);
+                        if (lista.length != 0) {
+                            detalle_tarea(lista)
+                        }
 
-	presentacion();
-	ingreso= ingreso_datos.questionInt("Dime Cual de estas opciones deseas:\n")
-	switch(ingreso){
-		case 1: 	
-			do{
-				CasoUno();
-				ingreso= ingreso_datos.questionInt("Dime Cual de estas opciones deseas:\n")
-				switch(ingreso){
-					case 1: 
-						verLista_todo(lista);
-						if(lista.length!=0){detalle_tarea(lista, seleccion)}
-						
-						
-					break;
-					case 2: 
-						verLista_pendiente(lista);
-						if(lista.length!=0){detalle_tarea(lista, seleccion)}
-						
-					break;
-					case 3:
-						verLista_curso(lista);
-						if(lista.length!=0){detalle_tarea(lista, seleccion)}
-						
-					break;
-					case 4: 
-						verLista_curso(lista);
-						if(lista.length!=0){detalle_tarea(lista, seleccion)}
-						
-					break;
-					case 5:
-						CondicionCasoUno=1; 
-					
-					break;
-					default: 
-						console.log("El valor Ingresado es incorrecto, intentelo de nuevo \n");
-					break;
-			
-				
-				}
-			
-			}while(CondicionCasoUno === 0);
-			
-			
-			break;
-		case 2: 
-			break;
-		case 3: 
-			lista.push(agregar_elemento());
-			break;
-		case 4: 
-			condicion=3;
-			console.log("Gracias por usarnos");
-			break;
-		default:
-		
-			console.log("El valor ingresado es incorrecto"); 
-			break;
-	
-	}
+                        break;
+                    case 2:
+                        verLista_pendiente(lista);
+                        if (lista.length != 0) {
+                            detalle_tarea(lista)
+                        }
 
+                        break;
+                    case 3:
+                        verLista_curso(lista);
+                        if (lista.length != 0) {
+                            detalle_tarea(lista)
+                        }
 
+                        break;
+                    case 4:
+                        verLista_curso(lista);
+                        if (lista.length != 0) {
+                            detalle_tarea(lista)
+                        }
 
+                        break;
+                    case 5:
+                        CondicionCasoUno = 1;
 
-}while(condicion === 0);
+                        break;
+                    default:
+                        console.log("El valor Ingresado es incorrecto, inténtelo de nuevo \n");
+                        break;
+                }
+            } while (CondicionCasoUno === 0);
+
+            break;
+        case 2:
+            //Esta linea crea otra lista conteniendo aquellos elementos que cumplan la busqueda en "lista" 
+            resultado_busqueda = lista.filter((objeto) => objeto.titulo.includes(ingreso_datos.question("Dime el titulo de la tarea que deseas buscar: \n"))); 
+            if (lista.length != 0) {
+                verLista_todo(resultado_busqueda);//reutilizo la funcion
+                detalle_tarea(lista)
+            }else{console.log("No hay ningun resultado en la agenda");}
+
+            break;
+        case 3:
+            lista.push(agregar_elemento());
+            break;
+        case 4:
+            condicion = 3;
+            console.log("Gracias por usar el programa");
+            break;
+        default:
+            console.log("El valor ingresado es incorrecto");
+            break;
+    }
+} while (condicion === 0);
--- tarea.js
+++ tarea.js
@@ -1,10 +1,10 @@
 const ingreso_teclado= require('readline-sync');
 class Tarea{
-	constructor(titulo,descripcion,estado=1,vencimiento,dificultad){
+	constructor(titulo,descripcion,vencimiento,dificultad){
 	
 		this.titulo=titulo;
 		this.descripcion=descripcion;
-		this.estado=estado;
+		this.estado=1;
 		this.creacion=new Date();
 		this.ultima_edicion=new Date();
 		this.vencimiento=vencimiento;
@@ -13,16 +13,17 @@ class Tarea{
 	}
 	///metodos
 	editar(){
-		titulo=ingreso_teclado.question("Titulo: \n");
-		descripcion=ingreso_teclado.question("Descripcion: \n");
-		estado=ingreso_teclado.questionInt("Estado(1pendiente,2en curso,3finalizado): \n");
-		ultima_edicion = new Date();
+		this.titulo=ingreso_teclado.question("Titulo: \n");
+		this.descripcion=ingreso_teclado.question("Descripcion: \n");
+		this.estado=ingreso_teclado.questionInt("Estado(1pendiente,2en curso,3finalizado): \n");
+		this.ultima_edicion = new Date();
 		do{
 			const fechaTexto = ingreso_teclado.question('Ingresa una fecha (YYYY-MM-DD): ');
 			// Intentar convertir la cadena a un objeto Date
-			vencimiento = new Date(fechaTexto);
-		}while(isNaN(vencimiento.getTime()));
-		Dificultad=ingreso_teclado.questionInt("Dificultad(1-facil 2medio 3dificil): \n"); 
+			this.vencimiento = new Date(fechaTexto);
+		}while(isNaN(this.vencimiento.getTime()));
+		this.Dificultad=ingreso_teclado.questionInt("Dificultad(1-facil 2medio 3dificil): \n"); 
+		console.log("Datos guardados!\n");
 	}
 }
 module.exports=Tarea;
